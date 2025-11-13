16190b20ffba39b4e2da230e2d53a5b8e8b7dbbd
Ringo Dulay <ringo.dulay@lomby.jp>
2025-11-13 11:52:09 +0900

    line string width 3

--- src/loader/ApiVectorLoader.js
+++ src/loader/ApiVectorLoader.js
@@ -69,7 +69,7 @@ export class ApiVectorLoader {
 
       const matLine = new LineMaterial({
         color: new THREE.Color().setRGB(...color),
-        linewidth: params.linewidth || 4,
+        linewidth: params.linewidth || 3,
         resolution: new THREE.Vector2(1000, 1000),
         dashed: false,
       });
